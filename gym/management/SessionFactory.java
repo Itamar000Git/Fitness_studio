8d4e42f7d83fac9acea7603cbe2b164f81594f86
itama <itamarbabai98@gmail.com>
2024-12-20 16:20:08 +0200

    to do list: 1. make sure solid princ' - done 2. chack taxt 3. comments

--- gym/management/SessionFactory.java
+++ gym/management/SessionFactory.java
@@ -10,10 +10,20 @@ class SessionFactory {
     public static Session createSession(SessionType type, String date, ForumType forum, Instructor i) {
         Session s = null;
         switch (type){
-            case MachinePilates -> s= new MachinePilatesSession(date,forum,i);
-            case Pilates -> s= new PilatesSession(date,forum,i);
-            case Ninja -> s= new NinjaSession(date,forum,i);
-            case ThaiBoxing -> s= new ThaiBoxingSession(date,forum,i);
+            case MachinePilates :
+                s= new MachinePilatesSession(date,forum,i);
+                break;
+            case Pilates :
+                s= new PilatesSession(date,forum,i);
+                break;
+            case Ninja :
+                s= new NinjaSession(date,forum,i);
+                break;
+            case ThaiBoxing :
+                s= new ThaiBoxingSession(date,forum,i);
+                break;
+            default:
+                throw new IllegalArgumentException("This gym don't have this kind of session");
         }
 
         return s;
